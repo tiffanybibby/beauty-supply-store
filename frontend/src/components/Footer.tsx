521c7ba3246c38f6c69f221db1ef974028a90488
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2022-01-10 19:40:55 -0500

    footer code

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import { Box, Typography, Link } from "@mui/material";
+
+const Footer = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        height: "120px",
+        backgroundColor: "rgb(5	59 136)",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography>
+        &copy; {new Date().getFullYear()}{" "}
+        <Link
+          style={{ color: "inherit" }}
+          href="https://github.com/tiffanydiandra/beauty-supply-store"
+        >
+          BSS.
+        </Link>{" "}
+        All Rights Reserved.
+      </Typography>
+    </Box>
+  );
+};
+
+export default Footer;
